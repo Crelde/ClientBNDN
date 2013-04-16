31f81b40fcc4bcffdc7fac1f154feb11a1c176c0
crelde <crelde@hotmail.com>
2013-04-16 16:41:13 +0200

    Added Controller class

---
+++ WebApplication1/Controller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    // IMPLEMENT LOTS OF METHODS
+    public class Controller : ServiceReference1.ServiceClient
+    {
+        // Use override keyword and go crazy
+        //override 
+
+    }
+}
